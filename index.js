289b333f5123ee2f4296d5dc3c68da070b52b220
mehrdad gharibdoost <gharibdoost@hotmail.com>
2019-11-07 11:20:41 +0330

    all ticker method

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const api = require("binance");
 const binanceWS = new api.BinanceWS(true);
 
+binanceWS.onAllTickers((data) => {
+    console.log(data);
+});
+
 binanceWS.onDepthUpdate('BNBBTC', (data) => {
     console.log(data);
 });
