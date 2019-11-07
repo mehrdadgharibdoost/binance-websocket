86263d9a7e2b625906d4547896071e877d6162e3
mehrdad gharibdoost <gharibdoost@hotmail.com>
2019-11-07 12:23:21 +0330

    add app & server

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const http = require("http").createServer();
+const io = require("socket.io")(http);
+const api = require("binance");
+
+const binanceWS = new api.BinanceWS(true);
+
+io.of("/live-price").on("connection", (socket) => {
+
+    binanceWS.onAllTickers((data) => {
+        io.of("/live-price").emit('tickerUpdate', data);
+    });
+
+    /* socket.emit("welcome", "hello dear user ...");
+    console.log("New Client is Connected ...");
+
+    socket.on("joinRoom", (room) => {
+        if (rooms.includes(room)) {
+            socket.join(room);
+            socket.emit("success", `you joins successfuly in room named: ${room}`);
+            io.of("/test").in(room).emit('newUser', 'new user joined to the room named: ' + room);
+        } else {
+            socket.emit("err", `the room named '${room}' is not exists`);
+        }
+
+    }); */
+});
+
+http.listen(port, () => {
+    console.log("Server is listening on 127.0.0.1:" + port);
+});
