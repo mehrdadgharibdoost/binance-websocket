d5faaef291b4660f7f964da86f9df8d5575ee42b
mehrdad gharibdoost <gharibdoost@hotmail.com>
2019-11-09 17:45:52 +0330

    get coins prices in their markets

--- server.js
+++ server.js
@@ -4,19 +4,73 @@ const port = 3000;
 const http = require("http").createServer();
 const io = require("socket.io")(http);
 const api = require("binance");
+const _array = require('lodash/array');
+const _collection = require('lodash/collection');
 
 const binanceWS = new api.BinanceWS(true);
 
-/* binanceWS.onAllTickers((data) => {
-    console.log(data.length);
-    // io.of("/live-price").emit('tickerUpdate', data['symbol']);
+binanceWS.onAllTickers((data) => {
+    /** get from DB */
+    var livePriceAssetsInfo = [
+        {
+            "asset": "btc",
+            "market": "btcusdt",
+            "market_unit": "usdt"
+        },
+        {
+            "asset": "bnb",
+            "market": "bnbbtc",
+            "market_unit": "btc"
+        },
+        {
+            "asset": "qkc",
+            "market": "qkcbtc",
+            "market_unit": "btc"
+        }
+    ];
+
+    // make array of markets for live price
+    var livePriceAssetsInfoMarkets = livePriceAssetsInfo.map(function (el) { return el.market.toUpperCase(); });
+
+    // get only needed results from binance
+    var results = _collection.filter(data, function (item) {
+        if (livePriceAssetsInfoMarkets.includes(item.symbol)) {
+            var livePriceAssetsInfoObj = livePriceAssetsInfo.find(obj => {
+                objMarket = obj.market.toUpperCase();
+                return objMarket === item.symbol;
+            });
+            item.asset = livePriceAssetsInfoObj.asset.toUpperCase();
+            item.market_unit = livePriceAssetsInfoObj.market_unit.toUpperCase();
+            return true;
+        } else {
+            return false;
+        }
+    });
+
+    // make ready output
+    var str = "";
+    results.forEach(element => {
+        str += `| ${element.asset} : ${element.currentClose} (${element.market_unit}) |`;
+    });
+    console.log(str);
+});
+
+/* binanceWS.onTicker("BTCUSDT", (data) => {
+    console.log(data.currentClose);
 }); */
+
+
 
 io.of("/live-price").on("connection", (socket) => {
 
     binanceWS.onAllTickers((data) => {
-        //console.log(data.length);
         io.of("/live-price").emit('tickerUpdate', data);
+
+        var results = _collection.filter(data, function (item) {
+            return item.VAL.indexOf("BTC") > -1 || item.VAL.indexOf("USDT") > -1;
+        });
+
+        console.log(results);
     });
 
     /* socket.emit("welcome", "hello dear user ...");
